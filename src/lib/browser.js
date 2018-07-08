ebaed8cdd19a40225ecc47e8dfebe245013d3eef
YangFan789 <YangFan789@outlook.com>
2018-07-08 12:40:19 +0800

    fix(lib/browser.js) Remove useless code lines

--- src/lib/browser.js
+++ src/lib/browser.js
@@ -1,9 +1,4 @@
 require('babel-polyfill')
-
-var hehehe = new Promise(function (resolve, reject) {})
-var bbbbb = 'jfksaldj'.startsWith('/')
-console.log(hehehe)
-console.log(bbbbb)
 require('./browser_loader')
 var JC = require('./core')
 require('../services/all')

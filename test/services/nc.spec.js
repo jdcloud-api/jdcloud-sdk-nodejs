5edb738aa1bd9168450e0c6fcd599eb88661a268
wangbibo1 <wangbibo@jd.com>
2019-10-18 10:06:33 +0800

    更新复杂配置示例

--- test/services/nc.spec.js
+++ test/services/nc.spec.js
@@ -1,14 +1,23 @@
-var NC = require('../../src/services/nc')
+var NC = require('../../src/services/nativecontainer')
 var config = require('config')
 var global = config.get('global')
 describe('JDCloud.NC', function () {
-    var nc = new NC({
+    var nc = new NATIVECONTAINER({
         credentials: {
             accessKeyId: global.accessKeyId,
             secretAccessKey: global.secretAccessKey
         },
+        endpoint: {
+            host: 'nativecontainer.internal.cn-north-1.jdcloud-api.com', //指定非默认Endpoint
+            protocol: 'http' //设置使用HTTP而不是HTTPS，vpc专用域名不支持HTTPS
+        },
+        'x-extra-header': { //指定额外header
+            "x-jdcloud-security-token" : "xxx", //要调用开启了MFA操作保护的接口需要传递
+            "x-jdcloud-content-sha256" : "xxx", //body过大，希望用此value替代对body进行哈希的过程
+            "MyOwn" : "xxx"
+        },
         version: {
-            nc: 'v1'
+            nativecontainer: 'v1'
         }
     })
     // var nc = new NC();
